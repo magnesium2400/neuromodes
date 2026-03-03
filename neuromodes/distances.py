959fa68b40d18e93a265d5228e8eaa801d1f4dfc
Mehul Gajwani <98678665+magnesium2400@users.noreply.github.com>
2026-03-03 11:28:27 +1100

    [DOC] add TODO comments only

--- neuromodes/distances.py
+++ neuromodes/distances.py
@@ -1,10 +1,12 @@
-
 import numpy as np
 from scipy.sparse import coo_matrix
 from lapy.diffgeo import tria_compute_gradient, tria_compute_divergence
 
-
 def geodesic_heat(geometry, src_vids, splu_heat, splu_stiffness):
+    # TODO : 
+    # 1. investigate timings on 32k mesh. 
+    # 2. ensure this functionality has (future?) compatibility with use_cholmod results (i.e. should not be specific to splu) 
+    # 3. investigate if modes can be used to improve splu_stiffness.solve -- in theory this can be approximated using a decomposition of divf
 
     shape = (geometry.v.shape[0],len(src_vids))
     # TODO : consider how to manage geodesic to any vertex vs geodesic to all vertices separately
@@ -24,5 +26,3 @@ def geodesic_heat(geometry, src_vids, splu_heat, splu_stiffness):
     vf -= np.min(vf, axis=0)
 
     return vf
-
-
