6d435ce95160902aaf21f900e83362dc372032ec
chrisadamsonmcri <ChristopherLeslie.Adamson@monash.edu>
2026-03-06 17:52:42 +1100

    Implemented and tested two sample T, one way ANOVA, ANOVA_Z methods.

--- neuromodes/mbm/mbm.py
+++ neuromodes/mbm/mbm.py
@@ -4,6 +4,9 @@
 Module for MBM.
 """
 
+# for debugging
+from IPython import embed
+
 import neuromodes.basis
 import palm
 import os
@@ -11,6 +14,8 @@ import nibabel
 import numpy
 import scipy.io
 import scipy.stats
+import scipy.linalg
+import line_profiler
 
 def fdr_bh(pvals, q=0.05, method='pdep', report='no'):
     """
@@ -169,35 +174,37 @@ def fdr_bh(pvals, q=0.05, method='pdep', report='no'):
 def mbm_estimate_p_val_tail(
         nullStat: numpy.ndarray,
         observedStat: float,
-        pThr: float
-
+        pThr: float,
+        stop=False
 ):
     # dimension and type checking
 
     #corr_mat1out = False
-
     if numpy.isnan(observedStat) and numpy.all(numpy.isnan(nullStat)):
         # no tail
         pValueTail = 1
         rev = True
+    
     elif numpy.max(nullStat) <= observedStat:
         # right tail
         pValueTail = 0
         rev = False
+    
     elif numpy.min(nullStat) >= observedStat:
         # left tail
         pValueTail = 0
         rev = True
+    
     elif numpy.ptp(nullStat) == 0:
         # left tail
         pValueTail = 1
         rev = True
+    
     else:
         rev = observedStat < numpy.median(nullStat)
         pValueTail = 2 * palm.palm_pareto(observedStat, nullStat, rev, pThr, False)[0]
         if isinstance(pValueTail, numpy.ndarray):
             pValueTail = pValueTail[0]
-
     
     return (float(pValueTail), rev)
 
@@ -205,7 +212,8 @@ def mbm_estimate_p_val_tail(
 def mbm_stat_map(
         inputMap: numpy.ndarray,
         designMatrix: numpy.ndarray,
-        statTest: str
+        statTest: str,
+        stop=False
 ):
     
     # dimension and type checking
@@ -224,16 +232,164 @@ def mbm_stat_map(
         statMap[numpy.isnan(statMap)] = 0
 
     elif statTest.lower() == "one way anova":
+
+        # % seperating measurement of each group
+        # yy = zeros(size(stat.designMatrix,1)/2, size(y,2), size(stat.designMatrix,2)); % preallocation space
+
+        yy = list()
+        
+        # for iCol = 1:size(stat.designMatrix,2)
+
+        #     yy(:,:,iCol) = y(stat.designMatrix(:,iCol) == 1,:);
+
+        # end
+
+        for z in range(designMatrix.shape[1]):
+            I = numpy.where(designMatrix[:, z] == 1)[0]
+            
+            yy.append(numpy.take(inputMap, I, axis=0))
+        
+        yy = numpy.dstack(yy)
+        
+        # % calculating the statistics
+        # statMap = zeros(1,nVertice);
+        statMap = numpy.zeros(numVertices)
+
+        for z in range(numVertices):
+            curYY = numpy.squeeze(numpy.take(yy, z, axis=1))
+            statistic, pValue = scipy.stats.f_oneway(curYY[:, 0], curYY[:, 1])
+            statMap[z] = pValue
+            
+        #for iVertice = 1:nVertice
+        #     [p, tbl, stats] = anova1(squeeze(yy(:,iVertice,:)), [], 'off');
+        #     statMap(iVertice) = tbl{2,5};
+        # end
+        # statMap(isnan(statMap)) = 1; % mean is 0 and variance is 0, which mean the everyone in the two groups are identical.
+
         pass
     elif statTest.lower() == "ancova_f":
+        # % Check if any column has only one unique value
+        # nCols = size(stat.designMatrix, 2);
+        #nCols = designMatrix.shape[1]
+        # mask = arrayfun(@(c) numel(unique(stat.designMatrix(:,c))) ~= 1, 1:nCols); % use the non-unique covariates
+        # mask is a boolean vector, mask[i] == 1 if the column contains more than one unique value
+
+        mask = numpy.any(designMatrix[0] != designMatrix, axis=0)
+        
+        # mask(1) = 0; % not using the diagnosis as covariate
+        mask[0] = False
+
+        # matrixX = zeros(size(stat.designMatrix,1), sum(mask)+2) ;
+        matrixX = numpy.zeros((designMatrix.shape[0], numpy.count_nonzero(mask) + 2))
+        
+        # matrixX(:,1) = double(stat.designMatrix(:,1)==1);
+        matrixX[:, 0] = numpy.double(designMatrix[:, 0] == 1)
+        # matrixX(:,2) = double(stat.designMatrix(:,1)~=1);
+        matrixX[:, 1] = numpy.double(designMatrix[:, 0] != 1)
+        # matrixX(:,3:end) = stat.designMatrix(:,mask);
+        matrixX[:, 2:] = numpy.compress(mask, designMatrix, axis=1)
+
+        matrixX = numpy.matrix(matrixX)
+        # B = (matrixX'*matrixX)\(matrixX'*y);
+        B = numpy.matrix(scipy.linalg.solve(matrixX.T * matrixX, matrixX.T * numpy.matrix(inputMap)))
+        # eres = y - matrixX*B;
+        eres = inputMap - matrixX * B
+
+        # DOF = size(matrixX,1) - size(matrixX,2);
+        DOF = matrixX.shape[0] - matrixX.shape[1]
+        # rvar = sum(eres.^2,1)/DOF;
+        rvar = numpy.sum(numpy.multiply(eres, eres), axis=0) / DOF
+        
+        # C = [1 -1 zeros(1,size(matrixX,2)-2)];
+        C = numpy.matrix(numpy.concatenate((numpy.array([1]), numpy.array([-1]), numpy.zeros(matrixX.shape[1] - 2))))
+        
+        # J = sum(C~=0,2)-1;
+        J = numpy.count_nonzero(C != 0) - 1
+        
+        # G = C*B;
+        G = C * B
+
+        # FMap = (G.*inv(C*inv(matrixX'*matrixX) *C').*G)./(J*rvar);
+        # this line produces warnings
+        D = J * rvar
+        DZero = (D == 0)
+        D[DZero] = 1
+        statMap = numpy.divide(numpy.multiply(numpy.multiply(G, scipy.linalg.inv(C * scipy.linalg.inv(matrixX.T * matrixX) * C.T)), G), numpy.matrix(D))
+        
+        # FMap(isnan(FMap)) = 1; % mean is 0 and variance is 0, which mean the everyone in the two groups are identical.
+        statMap[DZero] = 1
+
         pass
     elif statTest.lower() == "ancova_p":
         pass
+    elif statTest.lower() == 'ancova_z':
+        # % Check if any column has only one unique value
+        # nCols = size(stat.designMatrix, 2);
+        #nCols = designMatrix.shape[1]
+        # mask = arrayfun(@(c) numel(unique(stat.designMatrix(:,c))) ~= 1, 1:nCols); % use the non-unique covariates
+        # mask is a boolean vector, mask[i] == 1 if the column contains more than one unique value
+
+        mask = numpy.any(designMatrix[0] != designMatrix, axis=0)
+        
+        # mask(1) = 0; % not using the diagnosis as covariate
+        mask[0] = False
+
+        # matrixX = zeros(size(stat.designMatrix,1), sum(mask)+2) ;
+        matrixX = numpy.zeros((designMatrix.shape[0], numpy.count_nonzero(mask) + 2))
+        
+        # matrixX(:,1) = double(stat.designMatrix(:,1)==1);
+        matrixX[:, 0] = numpy.double(designMatrix[:, 0] == 1)
+        # matrixX(:,2) = double(stat.designMatrix(:,1)~=1);
+        matrixX[:, 1] = numpy.double(designMatrix[:, 0] != 1)
+        # matrixX(:,3:end) = stat.designMatrix(:,mask);
+        matrixX[:, 2:] = numpy.compress(mask, designMatrix, axis=1)
+
+        matrixX = numpy.matrix(matrixX)
+        # B = (matrixX'*matrixX)\(matrixX'*y);
+        B = numpy.matrix(scipy.linalg.solve(matrixX.T * matrixX, matrixX.T * numpy.matrix(inputMap)))
+        # eres = y - matrixX*B;
+        eres = inputMap - matrixX * B
+
+        # DOF = size(matrixX,1) - size(matrixX,2);
+        DOF = matrixX.shape[0] - matrixX.shape[1]
+        # rvar = sum(eres.^2,1)/DOF;
+        rvar = numpy.sum(numpy.multiply(eres, eres), axis=0) / DOF
+        
+        # C = [1 -1 zeros(1,size(matrixX,2)-2)];
+        C = numpy.matrix(numpy.concatenate((numpy.array([1]), numpy.array([-1]), numpy.zeros(matrixX.shape[1] - 2))))
+        
+        # J = sum(C~=0,2)-1;
+        J = numpy.count_nonzero(C != 0) - 1
+        
+        # G = C*B;
+        G = C * B
+
+        # FMap = (G.*inv(C*inv(matrixX'*matrixX) *C').*G)./(J*rvar);
+        # this line produces warnings
+        D = J * rvar
+        DZero = (D == 0)
+        D[DZero] = 1
+        FMap = numpy.divide(numpy.multiply(numpy.multiply(G, scipy.linalg.inv(C * scipy.linalg.inv(matrixX.T * matrixX) * C.T)), G), numpy.matrix(D))
+        
+        # FMap(isnan(FMap)) = 1; % mean is 0 and variance is 0, which mean the everyone in the two groups are identical.
+        FMap[DZero] = 1
+
+        # p = (1 - tcdf(sqrt(FMap), DOF))*2;
+        p = (1 - scipy.stats.t.cdf(numpy.sqrt(FMap), DOF)) * 2
+        
+        # p(p==0) = 10^-15;
+        p[p == 0] = 10e-15
+        #p = p + 10e-15 * (p == 0)
+
+        # z = norminv(1 - p/2);
+        z = scipy.stats.norm.ppf(1 - p / 2)
+        # statMap = z.*sign(G);
+        statMap = numpy.multiply(z, numpy.sign(G))
     else:
         raise ValueError('Unsupported statistical test')
     return statMap
 
-#@profile
+@line_profiler.profile
 def mbm(
         anatListFile: str,
         anatMaskFile: str,
@@ -253,21 +409,34 @@ def mbm(
     if not os.path.isfile(anatListFile):
         raise FileNotFoundError(anatListFile)
     
+    #print("loading started")
     anatInputMap = list()
 
     if anatListFile is not None:
-        FID = open(anatListFile, 'r')
-        anatList = [x.rstrip() for x in FID.readlines()]
-        FID.close()
+        head, tail = os.path.split(anatListFile)
+        fileName, ext = os.path.splitext(tail)
         
-        for curAnatFile in anatList:
-            if not os.path.isfile(curAnatFile):
-                anatInputMap.append(None)
-            else:
-                g = nibabel.load(curAnatFile)
-                anatInputMap.append(g.agg_data())
-                del g
-        anatInputMap = numpy.vstack(anatInputMap)
+        if ext == '.txt':
+            FID = open(anatListFile, 'r')
+            anatList = [x.rstrip() for x in FID.readlines()]
+            FID.close()
+            
+            for curAnatFile in anatList:
+                if not os.path.isfile(curAnatFile):
+                    anatInputMap.append(None)
+                else:
+                    g = nibabel.load(curAnatFile)
+                    anatInputMap.append(g.agg_data())
+                    del g
+            anatInputMap = numpy.vstack(anatInputMap)
+        elif ext == '.mgh':
+            MGH = nibabel.load(anatListFile)
+            anatInputMap = MGH.get_fdata()
+            #print(MGH)
+            exit()
+        elif ext == '.mat':
+            anatInputMap = scipy.io.loadmat(anatListFile)
+            anatInputMap = numpy.array(anatInputMap['inputMap'])
 
     if os.path.isfile(anatMaskFile):
         anatMask = numpy.loadtxt(anatMaskFile)
@@ -283,7 +452,9 @@ def mbm(
     if os.path.isfile(massFile):
         # geomMass = numpy.loadtxt(massFile, delimiter=',')
         geomMass = scipy.io.loadmat(massFile)
-        geomMass = numpy.float32(geomMass['mass'].toarray())
+        geomMass = scipy.sparse.csc_matrix(geomMass['mass'])
+
+    #print("loading ended")
     # end of mbm_read_inputs
     
     # mbm_check_read_inputs
@@ -320,9 +491,14 @@ def mbm(
     geomEig = numpy.take(numpy.compress(anatMask, geomEig, axis=0), numpy.arange(nEigenmodes), axis=1)
     
     #MBM.eig.mass = MBM.eig.mass(MBM.maps.mask == 1, MBM.maps.mask == 1);
-    geomMass = numpy.compress(anatMask, geomMass, axis=0)
-    geomMass = numpy.compress(anatMask, geomMass, axis=1)
+    #print(type(geomMass))
 
+    anatMaskIDX = numpy.where(anatMask)[0]
+    geomMass = geomMass[numpy.ix_(anatMaskIDX, anatMaskIDX)]
+    
+    #geomMass = numpy.compress(anatMask, geomMass, axis=0)
+    #geomMass = numpy.compress(anatMask, geomMass, axis=1)
+    #print(geomMass.shape)
     observedStatMap = mbm_stat_map(anatInputMap, statDesignMatrix, statTest)
 
     # mbm_perm_test_map
@@ -333,13 +509,14 @@ def mbm(
     numVertices = anatInputMap.shape[1]
     
     # test with trangs code
-    iNull = scipy.io.matlab.loadmat('../MBM/iNull.mat')
+    iNull = scipy.io.matlab.loadmat('../MBM/iNull_emp_two_sample_t.mat')
     iNull = numpy.array(iNull['iNull']) - 1
+    
     for permutationIDX in range(statNPer):
-            
-        if statTest.lower() == "one sample":
 
-            iSwap = (numpy.float32(numpy.random.uniform(size=(numSubjects, 1)) > 0.5) - 0.5) * 2
+        if statTest.lower() == "one sample":
+            iSwap = numpy.round(numpy.random.uniform(size=(numSubjects, 1)))
+            iSwap[iSwap == 0] = -1
             anatInputMapNull = anatInputMap * iSwap
 
             # the below code has a bug when numpy.sign == 0
@@ -348,17 +525,24 @@ def mbm(
         else:
             #iNull = numpy.argsort(numpy.random.uniform(size=numSubjects))
 
-            statNullDesignMatrix = numpy.take(statDesignMatrix, iNull[:, permutationIDX], axis=0)
+            #statNullDesignMatrix = numpy.take(statDesignMatrix, iNull, axis=0)
+            #statNullDesignMatrix = numpy.take(statDesignMatrix, iNull[:, permutationIDX], axis=0)
+            statNullDesignMatrix = numpy.array(statDesignMatrix)
+            
+            statNullDesignMatrix[:, 0] = statNullDesignMatrix[iNull[:, permutationIDX], 0]
+
             statMapNull.append(mbm_stat_map(anatInputMap, statNullDesignMatrix, statTest))
     statMapNull = numpy.vstack(statMapNull)
 
     permPMap = numpy.zeros(numVertices, dtype=numpy.float32)
     permRevMap = numpy.zeros(numVertices, dtype=numpy.bool_)
-
+    
+    observedStatMap = numpy.array(observedStatMap).ravel()
+    #print(observedStatMap.shape)
     for vertexIDX in range(numVertices):
-        permPMap[vertexIDX], permRevMap[vertexIDX] = mbm_estimate_p_val_tail(statMapNull[:, vertexIDX], observedStatMap[vertexIDX], statPThr)
+        permPMap[vertexIDX], permRevMap[vertexIDX] = mbm_estimate_p_val_tail(numpy.ravel(statMapNull[:, vertexIDX]), observedStatMap[vertexIDX], statPThr, stop=False)
         
-    if statFDR:
+    if statFDR and statPThr is not None:
         #h, crit_p, adj_ci_cvrg, permPMap = fdr_bh(permPMap, statPThr, 'pdep')
         permPMap = fdr_bh(permPMap, statPThr, 'pdep')[3]
         # del h
@@ -382,6 +566,7 @@ def mbm(
     
     
     #calc_eigendecomposition    
+    
     eigBeta = neuromodes.basis.decompose(observedStatMap, geomEig, 'project', geomMass, False)
 
     # mbm_perm_test_beta
@@ -391,7 +576,7 @@ def mbm(
 
     eigPBeta = numpy.zeros(nEigenmodes, dtype=numpy.float32)
     eigPRevBeta = numpy.zeros(nEigenmodes, dtype=numpy.bool_)
-
+    
     for z in range(nEigenmodes):
         eigPBeta[z], eigPRevBeta[z] = mbm_estimate_p_val_tail(betaNull[:, z], eigBeta[z], statPThr)
     
@@ -407,6 +592,7 @@ def mbm(
     significantEigBeta = numpy.array(eigBeta).ravel()
     significantEigBeta[eigPBeta > statPThr] = 0
     
+    # minus to sort descend
     betaOrderIDX = numpy.argsort(numpy.abs(significantEigBeta).ravel())
     betaOrderIDX = numpy.flip(betaOrderIDX)
     
@@ -436,20 +622,69 @@ def mbm(
 
 if __name__ == "__main__":
 
+    # demos from the github page
+    pass
     # mbm_demo_sim
 
-    f = mbm(
-        anatListFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'inputMaps_full_path.txt'),
-        anatMaskFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'mask_S1200.L.midthickness_MSMAll.32k_fs_LR.txt'),
-        statTest='two sample',
-        statDesignFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'G_onewayANOVA_twosample.txt'),
-        statNPer=10,
-        statPThr=0.1,
-        statThresh=0.05,
-        statFDR=True,
-        eigFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_emode_150.mat'),
-        massFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_mass_150.mat'),
-        nEigenmodes=150
-    )
-    #f['geomMass'] = numpy.float32(f['geomMass'])
-    scipy.io.matlab.savemat('neuromodes_mbm.mat', f)
+    # f = mbm(
+    #     anatListFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'inputMaps_full_path.txt'),
+    #     anatMaskFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'mask_S1200.L.midthickness_MSMAll.32k_fs_LR.txt'),
+    #     statTest='two sample',
+    #     statDesignFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'G_onewayANOVA_twosample.txt'),
+    #     statNPer=10,
+    #     statPThr=0.1,
+    #     statThresh=0.05,
+    #     statFDR=True,
+    #     eigFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_emode_150.mat'),
+    #     massFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_mass_150.mat'),
+    #     nEigenmodes=150
+    # )
+    #scipy.io.matlab.savemat('neuromodes_mbm.mat', f)
+    # f = mbm(
+    #     anatListFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'inputMaps_full_path.txt'),
+    #     anatMaskFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'mask_S1200.L.midthickness_MSMAll.32k_fs_LR.txt'),
+    #     statTest='one way anova',
+    #     statDesignFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'G_onewayANOVA_twosample.txt'),
+    #     statNPer=10,
+    #     statPThr=0.1,
+    #     statThresh=0.05,
+    #     statFDR=True,
+    #     eigFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_emode_150.mat'),
+    #     massFile=os.path.join('..', 'MBM', 'data', 'demo_sim', 'fsLR_32k_midthickness-lh_mass_150.mat'),
+    #     nEigenmodes=150
+    # )
+    # scipy.io.matlab.savemat('neuromodes_mbm_sim_one_way_anova.mat', f)
+    # mbm_demo_emp
+    # f = mbm(
+    #     anatListFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'inputMaps_ANCOVA_twosample.mat'),
+    #     anatMaskFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_cortex-lh_mask.txt'),
+    #     statTest='ANCOVA_Z',
+    #     statDesignFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'G_ANCOVA.txt'),
+    #     statNPer=1000,
+    #     statPThr=0.1,
+    #     statThresh=0.05,
+    #     statFDR=False,
+    #     eigFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_midthickness-lh_emode_200.mat'),
+    #     massFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_midthickness-lh_mass_200.mat'),
+    #     nEigenmodes=200
+    # )
+    # #f['geomMass'] = numpy.float32(f['geomMass'])
+    # scipy.io.matlab.savemat('neuromodes_mbm_emp_ancova_z.mat', f)
+
+    # f = mbm(
+    #     anatListFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'inputMaps_ANCOVA_twosample.mat'),
+    #     anatMaskFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_cortex-lh_mask.txt'),
+    #     statTest='two sample',
+    #     statDesignFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'G_two_sample.txt'),
+    #     statNPer=1000,
+    #     statPThr=0.1,
+    #     statThresh=0.05,
+    #     statFDR=False,
+    #     eigFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_midthickness-lh_emode_200.mat'),
+    #     massFile=os.path.join('..', 'MBM', 'data', 'demo_emp', 'fsaverage_164k_midthickness-lh_mass_200.mat'),
+    #     nEigenmodes=200
+    # )
+    # #f['geomMass'] = numpy.float32(f['geomMass'])
+    # scipy.io.matlab.savemat('neuromodes_mbm_emp_two_sample_t.mat', f)
+    
+    
