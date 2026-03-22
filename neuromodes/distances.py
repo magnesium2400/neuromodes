01affe9436b3d3e8ff6f1760b4168db2cd6101c3
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2026-03-23 00:10:51 +1100

    [WIP] distances parallel on lapy now

--- neuromodes/distances.py
+++ neuromodes/distances.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import splu
+from lapy import Solver
 from lapy.diffgeo import tria_compute_gradient, tria_compute_divergence
 
 def geodesic_heat(geometry, src_vids, splu_heat, splu_stiffness):
@@ -14,9 +16,7 @@ def geodesic_heat(geometry, src_vids, splu_heat, splu_stiffness):
                     
     vfunc = splu_heat.solve(b0)
 
-    # Speeding this up would require changes to LaPy's tria_compute_gradient/_divergence to support parallel computation
-    divf = np.zeros_like(b0)
-    # for i in range(len(src_vids)): 
+    # LaPy now supports parallel computation of grads and divs (1.6.0+)
     gradf = tria_compute_gradient(geometry, vfunc)
     gradnorm = gradf / np.sqrt((gradf**2).sum(-1))[...,np.newaxis]
     gradnorm = np.nan_to_num(gradnorm)
@@ -26,3 +26,11 @@ def geodesic_heat(geometry, src_vids, splu_heat, splu_stiffness):
     vf -= np.min(vf, axis=0)
 
     return vf
+
+def geodesic_setup(geometry, m = 1.0):
+    a, b = Solver._fem_tria(geometry, lump=True)
+    splu_stiffness = splu(a)
+    t = m * geometry.avg_edge_length() ** 2
+    hmat = b + t * a
+    splu_heat = splu(hmat)
+    return dict(splu_heat=splu_heat, splu_stiffness=splu_stiffness, geometry=geometry)
