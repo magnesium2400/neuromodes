0d141dcef10698c38b5065b250eb905d08447291
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2026-03-23 12:30:53 +1100

    [WIP] mesh add estimate_fwhm_fem

--- neuromodes/mesh.py
+++ neuromodes/mesh.py
@@ -1,15 +1,16 @@
 from typing import Union
 import numpy as np
-from lapy import TriaMesh
+from lapy import TriaMesh, Solver
 from numpy.typing import ArrayLike
 
 from neuromodes.eigen import get_eigengroup_inds
 from neuromodes.basis import decompose
 
-def estimate_fwhm(geometry: TriaMesh, vfunc, mask=None):
+def estimate_fwhm(geometry: TriaMesh, vfunc, roi_mask=None):
+    """Equivalent to wb_command -metric-estimate-fwhm"""
     # Prelims
-    rois = np.ones(len(geometry.v), dtype=bool) if mask is None else np.asarray(mask, dtype=bool)
-    data = vfunc[rois, :] if vfunc.ndim > 1 else vfunc[rois, np.newaxis]        
+    rois = np.ones(len(geometry.v), dtype=bool) if roi_mask is None else np.asarray(roi_mask, dtype=bool)
+    data = vfunc[rois, :] if vfunc.ndim > 1 else vfunc[rois, np.newaxis]
 
     # Get edges
     adj = geometry.adj_sym[rois, :][:, rois]
@@ -22,6 +23,14 @@ def estimate_fwhm(geometry: TriaMesh, vfunc, mask=None):
     # In accordance with wb_command, use whole mesh mean edge length (not just mask)
     fwhm = geometry.avg_edge_length() * np.sqrt(-2 * np.log(2) / np.log(1 - vl / (2 * vg)))
 
+    return fwhm
+
+def estimate_fwhm_fem(geometry: Solver, vfunc):
+    vf = vfunc - np.average(vfunc, weights=geometry.mass.diagonal(), axis=0) # set (mass-weighted) mean to 0
+    Vm = np.sum(vf * geometry.mass.dot(vf), axis=0)       # diag(vf.T @ M @ vf) 
+    Vs = np.sum(vf * geometry.stiffness.dot(vf), axis=0)  # diag(vf.T @ S @ vf)
+    lambda_eff = Vs / Vm
+    fwhm = np.sqrt(8 * np.log(2) / lambda_eff)
     return fwhm
      
 def truncate_emodes(geometry: TriaMesh, vfunc, emodes, evals, mass, per_spectrum=None): 
@@ -104,3 +113,21 @@ def group_to_mode(group_id: ArrayLike, method: str = 'ceil') -> Union[int, float
     # Calcs
     result = func(np.asarray(group_id) + 1).astype(outtype)**2 - 1
     return result.item() if np.isscalar(group_id) else result
+
+def wavelength_from_eigengroup_sphere(group_id, radius=1):
+    return 2 * np.pi * radius / np.sqrt(group_id * (group_id + 1))
+
+def eigenvalue_from_eigengroup_sphere(group_id, radius=1):
+    return group_id * (group_id + 1) / radius**2
+
+def fwhm_from_eigengroup_sphere(group_id, radius=1):
+    return np.sqrt(8*np.log(2)) * radius / np.sqrt(group_id * (group_id + 1))
+
+def wavelength_from_fwhm(fwhm):
+    return np.pi / np.sqrt(2*np.log(2)) * fwhm
+
+def wavelength_from_eigenvalue(evals):
+    return 2 * np.pi / np.sqrt(evals)
+
+def eigenvalue_from_surface_area(mode_id, area):
+    return 4 * np.pi * mode_id / area
