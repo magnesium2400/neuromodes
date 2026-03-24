42a32e1293f3102d4bac3805cf61498d790a6001
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2026-03-24 12:16:20 +1100

    [WIP] first complete draft of truncate_emodes

--- neuromodes/mesh.py
+++ neuromodes/mesh.py
@@ -1,12 +1,39 @@
 from typing import Union
+from warnings import warn
 import numpy as np
 from lapy import TriaMesh, Solver
 from numpy.typing import ArrayLike
 
-from neuromodes.eigen import get_eigengroup_inds
-from neuromodes.basis import decompose
+from neuromodes.eigen import EigenSolver
+from neuromodes.basis import decompose, reconstruct
 
-def estimate_fwhm(geometry: TriaMesh, vfunc, roi_mask=None):
+def estimate_fwhm(surf: TriaMesh | Solver | EigenSolver, vfunc, method='fem', roi_mask=None, output='fwhm'):
+    if method == 'wb':
+        if isinstance(surf, TriaMesh):
+            return _estimate_fwhm_wb(surf, vfunc, roi_mask, output=output)
+        elif isinstance(surf, EigenSolver): 
+            return _estimate_fwhm_wb(surf.geometry, vfunc, roi_mask, output=output) 
+        elif isinstance(surf, Solver):
+            raise ValueError("Solver instances cannot be used as they do not have a geometry attribute.")
+        else:
+            raise ValueError("For 'wb' method, geometry must be a TriaMesh or EigenSolver instance.")
+        
+    elif method == 'fem':
+        if roi_mask is not None:
+            warn("roi_mask is not currently supported for 'fem' method and will be ignored.")
+
+        if isinstance(surf, Solver) or isinstance(surf, EigenSolver):
+            stiffness, mass = surf.stiffness, surf.mass
+        elif isinstance(surf, TriaMesh):
+            stiffness, mass = Solver._fem_tria(surf) # TODO : Check lump (true? false? either? default?)
+        else:
+            raise ValueError("For 'fem' method, geometry must be either a Solver instance or a TriaMesh instance.")
+        return _estimate_fwhm_fem(mass, stiffness, vfunc, output=output)
+    
+    else:
+        raise ValueError("Method must be either 'wb' or 'fem'.")
+
+def _estimate_fwhm_wb(geometry: TriaMesh, vfunc, roi_mask=None, output='fwhm'):
     """Equivalent to wb_command -metric-estimate-fwhm"""
     # Prelims
     rois = np.ones(len(geometry.v), dtype=bool) if roi_mask is None else np.asarray(roi_mask, dtype=bool)
@@ -22,35 +49,68 @@ def estimate_fwhm(geometry: TriaMesh, vfunc, roi_mask=None):
     vl = np.mean((data[rows, :] - data[cols, :])**2, axis=0)  # local variance
     # In accordance with wb_command, use whole mesh mean edge length (not just mask)
     fwhm = geometry.avg_edge_length() * np.sqrt(-2 * np.log(2) / np.log(1 - vl / (2 * vg)))
+    
+    lambda_eff = convert_to_eigenvalue(fwhm, input_type='fwhm')
+    return convert_from_eigenvalue(lambda_eff, output_type=output)
 
-    return fwhm
+def _estimate_fwhm_fem(mass, stiffness, vfunc, output='fwhm'):
+    # Vm = \Sigma_{i=1}^N \beta_i^2 (where \beta_i is the coefficient of mode i in the decomposition of vfunc)
+    # Vs = \Sigma_{i=1}^N \beta_i^2 \lambda_i (where \lambda_i is the eigenvalue of mode i)
+    # Vs/Vm = \lambda_{eff} where lambda is the effective eigenvalue of the map (weighted average)
+    # If the input is an mode i, then this reduces to \lambda_i for that mode
+    vf = vfunc - np.average(vfunc, weights=mass.diagonal(), axis=0) # set (mass-weighted) mean to 0
+    Vm = vf * (mass @ vf)
+    Vs = vf * (stiffness @ vf) - 0.5 * (stiffness @ vf**2)
+    lambda_eff = np.sum(Vs, axis=0) / np.sum(Vm, axis=0)
+    return convert_from_eigenvalue(lambda_eff, output_type=output)
 
-def estimate_fwhm_fem(geometry: Solver, vfunc):
-    vf = vfunc - np.average(vfunc, weights=geometry.mass.diagonal(), axis=0) # set (mass-weighted) mean to 0
-    Vm = np.sum(vf * geometry.mass.dot(vf), axis=0)       # diag(vf.T @ M @ vf) 
-    Vs = np.sum(vf * geometry.stiffness.dot(vf), axis=0)  # diag(vf.T @ S @ vf)
+def _estimate_fwhm_fem_local(mass, stiffness, vfunc, output='fwhm'):
+    vf = vfunc - np.average(vfunc, weights=mass.diagonal(), axis=0) # set (mass-weighted) mean to 0
+    Vm = vf * (mass @ vf)
+    Vs = vf * (stiffness @ vf) - 0.5 * (stiffness @ vf**2)
     lambda_eff = Vs / Vm
-    fwhm = np.sqrt(8 * np.log(2) / lambda_eff)
-    return fwhm
+    return convert_from_eigenvalue(lambda_eff, output_type=output)
      
-def truncate_emodes(geometry: TriaMesh, vfunc, emodes, evals, mass, per_spectrum=None): 
+def truncate_emodes(geometry: TriaMesh, vfunc, evals, emodes=None, mass=None, 
+                    error_threshold=None, power_threshold=None, wavelength_threshold=None, 
+                    reconstruct_kwargs={}, output='group'): 
     # TODO : probably change this so that per_spectrum represents total accuracy (eg from reconstruct)
     # rather than the percentage of the spectrum
     # TODO : check that the number of modes is not just the max (error if j is at the end)
     # TODO : add option to return exact mode vs. complete group 
-    if per_spectrum is not None:
-        power = decompose(data=vfunc, emodes=emodes, mass=mass)**2
-        power = power/np.sum(power)
-        n_mode = np.searchsorted(np.cumsum(power), per_spectrum, side='right')
-        n_group = mode_to_group(n_mode, method='ceil')
-        return group_to_mode(n_group, method='ceil')
+    if error_threshold is not None:
+        if emodes is None or mass is None:
+            raise ValueError("emodes and mass must be provided when using error_threshold.")
+        _, errors, _ = reconstruct(data=vfunc, emodes=emodes, mass=mass, **reconstruct_kwargs)
+        errors = np.squeeze(errors)
+        if error_threshold < errors[-1]: 
+            raise RuntimeError(f"Error threshold {error_threshold} is too low. Consider providing more modes or increasing threshold.")
+        n_mode = np.searchsorted(-errors, -error_threshold, side='right')
+    elif power_threshold is not None:
+        if emodes is None or mass is None:
+            raise ValueError("emodes and mass must be provided when using power_threshold.")
+        vf = vfunc - np.average(vfunc, weights=mass.diagonal(), axis=0)
+        total_power = vf.T @ mass @ vf
+        total_threshold = total_power * (1 - power_threshold)
+        betas = decompose(vf, emodes=emodes, mass=mass)
+        power = np.cumsum(betas**2)
+        if power[-1] < total_threshold:
+            raise RuntimeError(f"Power threshold {power_threshold} is too low. Consider providing more modes or increasing threshold.")
+        n_mode = np.searchsorted(power, total_threshold, side='right')
     else: 
-        fwhm = estimate_fwhm(geometry, vfunc)
-        with np.errstate(divide='ignore'):
-            wavelengths = 2 * np.pi / np.sqrt(evals)
-        n_mode = np.searchsorted(-wavelengths, -fwhm, side='right')
-        n_group = mode_to_group(n_mode, method='round')
-        return group_to_mode(n_group, method='ceil') # make it the whole group
+        if wavelength_threshold is None:
+            wavelength_threshold = estimate_fwhm(geometry, vfunc, output='wavelength')
+        wavelengths = convert_from_eigenvalue(evals, output_type='wavelength')
+        if wavelength_threshold < wavelengths[-1]:
+            raise RuntimeError(f"Wavelength threshold ({wavelength_threshold}) is smaller than the wavelength of the highest mode ({wavelengths[-1]}). Please provide more modes.")
+        n_mode = np.searchsorted(-wavelengths, -wavelength_threshold, side='right')
+
+    if output == 'mode': 
+        return n_mode+1
+    elif output == 'group':
+        return mode_to_group(n_mode, method='ceil')+1
+    else: 
+        raise ValueError("Output must be either 'mode' or 'group'.")
 
 # TODO : error if inputs are not integer
 # TODO : clarify that for inputs of the form n^2-1, all methods return n-1 
@@ -114,20 +174,41 @@ def group_to_mode(group_id: ArrayLike, method: str = 'ceil') -> Union[int, float
     result = func(np.asarray(group_id) + 1).astype(outtype)**2 - 1
     return result.item() if np.isscalar(group_id) else result
 
-def wavelength_from_eigengroup_sphere(group_id, radius=1):
-    return 2 * np.pi * radius / np.sqrt(group_id * (group_id + 1))
+def convert_to_eigenvalue(value, input_type='fwhm', area=None):
+    if input_type == 'eigenvalue': 
+        return value
+    elif input_type == 'wavelength':
+        return (2 * np.pi / value)**2
+    elif input_type == 'fwhm':
+        return 8 * np.log(2) / value**2
+    elif input_type == 'group':
+        if area is None:
+            raise ValueError("Area must be provided when input_type is 'group'.")
+        return value * (value + 1) * 4 * np.pi / area
+    elif input_type == 'mode':
+        if area is None:
+            raise ValueError("Area must be provided when input_type is 'mode'.")
+        return 4 * np.pi * value / area 
+    else:
+        raise ValueError("Incorrect input_type specified")
 
-def eigenvalue_from_eigengroup_sphere(group_id, radius=1):
-    return group_id * (group_id + 1) / radius**2
+def convert_from_eigenvalue(eigenvalue, output_type='fwhm', area=None):
+    if output_type == 'eigenvalue': 
+        return eigenvalue
+    elif output_type == 'wavelength':
+        with np.errstate(divide='ignore'):
+            return 2 * np.pi / np.sqrt(eigenvalue)
+    elif output_type == 'fwhm':
+        with np.errstate(divide='ignore'):
+            return np.sqrt(8 * np.log(2) / eigenvalue)
+    elif output_type == 'group':
+        if area is None:
+            raise ValueError("Area must be provided when output_type is 'group'.")
+        return (np.sqrt(eigenvalue * area / np.pi + 1) - 1) / 2
+    elif output_type == 'mode':
+        if area is None:
+            raise ValueError("Area must be provided when output_type is 'mode'.")
+        return eigenvalue * area / (4 * np.pi)
+    else:
+        raise ValueError("Incorrect output_type specified")
 
-def fwhm_from_eigengroup_sphere(group_id, radius=1):
-    return np.sqrt(8*np.log(2)) * radius / np.sqrt(group_id * (group_id + 1))
-
-def wavelength_from_fwhm(fwhm):
-    return np.pi / np.sqrt(2*np.log(2)) * fwhm
-
-def wavelength_from_eigenvalue(evals):
-    return 2 * np.pi / np.sqrt(evals)
-
-def eigenvalue_from_surface_area(mode_id, area):
-    return 4 * np.pi * mode_id / area
--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import numpy as np
 from lapy import TriaMesh
 
-from neuromodes.mesh import estimate_fwhm, mode_to_group, group_to_mode
+from neuromodes.eigen import EigenSolver
+from neuromodes.mesh import estimate_fwhm, mode_to_group, group_to_mode, truncate_emodes
 from neuromodes.io import fetch_surf, fetch_map
 
-def test_fwhm_regression():
+def test_fwhm_wb():
+    ATOL = 1e-6
     # Do some CSV parsing without using pandas
     filename = Path(__file__).parent / 'test_data' / 'mesh_estimate_fwhm_results.csv'
     with open(filename, mode='r', newline='') as csv_file:
@@ -28,10 +30,19 @@ def test_fwhm_regression():
         mesh, _ = fetch_surf(surf_type=surf_type, template=template, species=species, density=density, hemi=hemi)
         geometry = TriaMesh(mesh.vertices, mesh.faces)
         vfunc = fetch_map(data=data, template=template, species=species, density=density, hemi=hemi)
-        estimated_fwhm = estimate_fwhm(geometry, vfunc)
+        estimated_fwhm = estimate_fwhm(geometry, vfunc, method='wb')
 
-        assert np.isclose(estimated_fwhm, float(expected_fwhm), atol=0.01), \
+        assert np.allclose(estimated_fwhm, float(expected_fwhm), atol=ATOL), \
             f"Estimated FWHM ({estimated_fwhm}) does not match expected FWHM ({float(expected_fwhm)}) for {data}"
+
+@pytest.mark.parametrize("method_and_rtol", [('wb', 1e-1), ('fem', 1e-6)])
+def test_fwhm(method_and_rtol):
+    mesh, _ = fetch_surf(surf_type='sphere', density='4k')
+    solver = EigenSolver(mesh).solve(n_modes=25)
+    estimated_value = estimate_fwhm(solver.geometry, solver.emodes[:,1:], method=method_and_rtol[0])
+    theoretical_value = np.sqrt(8 * np.log(2) / solver.evals[1:])
+    assert np.allclose(estimated_value, theoretical_value, rtol=method_and_rtol[1]), \
+        f"Estimated value does not match theoretical value for method {method_and_rtol[0]}"
 
 def test_mode_to_group():
     idx = np.arange(22)
@@ -59,15 +70,34 @@ def test_group_to_mode():
     assert np.allclose(group_to_mode(idx, method='raw'), 
         np.arange(22))
 
-def test_continuous_bijection():
+def test_mode_to_mode():
     """Tests that the 'raw' methods are perfectly invertible bijections."""
-    
     # Mode -> Group -> Mode
     a = np.arange(0, 100)
     b = group_to_mode(mode_to_group(a, method='raw'), method='raw')
     np.testing.assert_array_almost_equal(a, b)
 
+def test_group_to_group():
+    """Tests that the 'raw' methods are perfectly invertible bijections."""
     # Group -> Mode -> Group
     a = np.linspace(0, 20, 100)
     b = mode_to_group(group_to_mode(a, method='raw'), method='raw')
     np.testing.assert_array_almost_equal(a, b)
+
+@pytest.fixture(scope="module")
+def solver(): 
+    mesh, _ = fetch_surf(surf_type='sphere', density='4k')
+    return EigenSolver(mesh).solve(n_modes=25)
+
+def test_truncate_emodes(solver):
+    betas = np.random.randn(solver.n_modes, 1)
+    for n_zeros in range(1, solver.n_modes-2):
+        # Create data using only part of the mode basis
+        vfunc = solver.emodes @ np.vstack((betas[:-n_zeros], np.zeros((n_zeros,1))))
+        expected_groups = np.ceil(np.sqrt(solver.n_modes - n_zeros)).astype(int)
+        
+        # Expected and actual outputs
+        out = truncate_emodes(geometry=solver.geometry, vfunc=vfunc, 
+            emodes=solver.emodes, evals=solver.evals, mass=solver.mass, power_threshold=1e-8)
+        assert out == expected_groups, \
+            f"Expected {expected_groups} groups, but got {out} groups when truncating {n_zeros} modes."
